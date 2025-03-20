b889976e41b7206499732f76e30d657b01851540
EmreEkin <emreekin28@gmail.com>
2025-03-20 13:30:44 +0300

    ilk

---
+++ README.md
@@ -0,0 +1,62 @@
+# ARP Scan - MAC Adresi Taraması
+Bu Go programı, belirli bir IP aralığında ARP taraması yaparak, her cihazın MAC adresini tespit eder. Ayrıca, isteğe bağlı olarak, her MAC adresinin üretici bilgisini (vendor) de sorgular ve ekrana yazdırır. Program, ağınızdaki aktif cihazları kolayca tespit etmenize olanak tanır.
+
+### Özellikler
+ARP Taraması: Verilen IP aralığındaki cihazların MAC adreslerini çözümler.
+MAC Vendor Bilgisi: -vendor bayrağı ile MAC adresinin üretici bilgisini sorgular.
+Esnek IP Aralığı: IP aralığını komut satırından belirtme imkânı sunar.
+Yardım Bayrağı: Kullanıcıya komut satırında kullanım talimatlarını gösterir.
+Kullanım
+Program, terminal üzerinden çalıştırılabilir ve çeşitli bayraklarla kontrol edilebilir.
+
+### Komut Yapısı:
+
+./arp_scan [seçenekler]
+Seçenekler:
+-range <IP aralığı>: Taranacak IP aralığını belirtir. Varsayılan olarak 192.168.1.0/24 kullanılır.
+-vendor: MAC adresinin üretici bilgilerini gösterir. Bu bayrak belirtilmezse, sadece IP ve MAC adresi yazdırılır.
+-h: Kullanım bilgilerini gösterir.
+Örnek Kullanımlar:
+Sadece IP ve MAC Adreslerini Gösterme: Belirtilen IP aralığındaki cihazların IP ve MAC adreslerini görmek için:
+
+
+```
+./arp_scan -range 192.168.1.0/24
+Bu komut, IP aralığındaki her cihazın IP ve MAC adreslerini listeler.
+```
+MAC Vendor Bilgisiyle Gösterme: Eğer her cihazın MAC adresinin üretici bilgisini de görmek isterseniz:
+
+```
+
+./arp_scan -range 192.168.1.0/24 -vendor
+Bu komut, her cihazın IP ve MAC adresinin yanı sıra, MAC adresinin üreticisini (vendor) de listeler.
+```
+Yardım Bilgilerini Gösterme: Kullanım talimatlarını görmek için:
+
+```
+
+./arp_scan -h
+Bu komut, programın nasıl kullanılacağı hakkında bilgi verir.
+```
+### Örnek Çıktılar:
+MAC Vendor Bilgisi ile Çıktı:
+
+```
+
+IP: 192.168.1.1 - MAC: 00:11:22:33:44:55 - Üretici: Cisco Systems
+IP: 192.168.1.2 - MAC: 66:77:88:99:AA:BB - Üretici: TP-Link Technologies
+
+```
+```
+IP: 192.168.1.1 - MAC: 00:11:22:33:44:55
+IP: 192.168.1.2 - MAC: 66:77:88:99:AA:BB
+```
+### Kurulum
+
+```
+go get github.com/mdlayher/arp
+
+go build arp_scan.go
+
+./arp_scan -range 192.168.1.0/24 -vendor
+```
---
+++ arpscan.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
+	"net/http"
+
+	"github.com/mdlayher/arp"
+)
+
+type MacVendorResponse struct {
+	Vendor string `json:"vendor"`
+}
+
+func getMacVendor(mac string) (string, error) {
+	url := fmt.Sprintf("https://api.macvendors.com/%s", mac)
+	resp, err := http.Get(url)
+	if err != nil {
+		return "Bilinmiyor", err
+	}
+	defer resp.Body.Close()
+
+	var result MacVendorResponse
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return "Bilinmiyor", err
+	}
+
+	return result.Vendor, nil
+}
+
+func getDefaultInterface() (*net.Interface, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, iface := range interfaces {
+		if iface.Flags&net.FlagUp != 0 && iface.Flags&net.FlagLoopback == 0 {
+			return &iface, nil
+		}
+	}
+	return nil, fmt.Errorf("uygun ağ arayüzü bulunamadi")
+}
+
+func parseIPRange(rangeStr string) ([]string, error) {
+	parts := strings.Split(rangeStr, ".")
+	if len(parts) != 4 {
+		return nil, fmt.Errorf("gecersiz IP araligi formati")
+	}
+
+	base := strings.Join(parts[:3], ".")
+	var ips []string
+	for i := 1; i <= 254; i++ {
+		ips = append(ips, fmt.Sprintf("%s.%d", base, i))
+	}
+
+	return ips, nil
+}
+
+func main() {
+	// Flag
+	rangeFlag := flag.String("range", "192.168.1.0/24", "Taranacak IP araligi belirle (örn: 192.168.1.0/24)")
+	showVendor := flag.Bool("vendor", false, "MAC üretici bilgisini göster")
+	help := flag.Bool("h", false, "Kullanim bilgilerini göster")
+	flag.Parse()
+
+	// Yardım 
+	if *help {
+		fmt.Println("Kullanim: ./arp_scan [seçenekler]")
+		fmt.Println("Seçenekler:")
+		fmt.Println("  -vendor  MAC üretici bilgisini göster")
+		fmt.Println("  -range   Taranacak IP araligi belirle (örn: 192.168.1.0/24)")
+		fmt.Println("  -h       Kullanim bilgilerini göster")
+		os.Exit(0)
+	}
+
+	// Ağ arayüzü
+	iface, err := getDefaultInterface()
+	if err != nil {
+		log.Fatalf("Ağ arayüzü bulunamadi: %v", err)
+	}
+	fmt.Printf("Kullanilan arayüz: %s\n", iface.Name)
+
+	// ARP istemcisi
+	client, err := arp.New(iface)
+	if err != nil {
+		log.Fatalf("ARP istemcisi oluşturulamadi: %v", err)
+	}
+	defer client.Close()
+
+	// IP araligi parse
+	ips, err := parseIPRange(*rangeFlag)
+	if err != nil {
+		log.Fatalf("Geçersiz IP araligi: %v", err)
+	}
+
+	// IP araligi
+	for _, ip := range ips {
+		parsedIP := net.ParseIP(ip)
+		mac, err := client.Resolve(parsedIP)
+		if err != nil {
+			continue 
+		}
+
+		//MAC adresi ve IP araligi
+		if *showVendor {
+			vendor, err := getMacVendor(mac.String())
+			if err != nil {
+				vendor = "Bilinmiyor"
+			}
+			fmt.Printf("IP: %s - MAC: %s - Üretici: %s\n", ip, mac, vendor)
+		} else {
+			fmt.Printf("IP: %s - MAC: %s\n", ip, mac)
+		}
+
+		time.Sleep(50 * time.Millisecond)
+	}
+}
